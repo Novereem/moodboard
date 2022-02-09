262e38740359e5f5ce744d79c9d2c283ee31d91e
Stijn Verhagen <stijn@moonlysoftware.com>
2022-02-09 13:17:24 +0100

    Added Tailwind and first components.

--- src/App.js
+++ src/App.js
@@ -1,23 +1,20 @@
 import logo from './logo.svg';
 import './App.css';
+import Card from './components/Card.js';
+import ImageCard from './components/ImageCard.js';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="App flex justify-center"  data-theme="mytheme">
+      <div className='grid'>    
+          <Card></Card>
+          <ImageCard></ImageCard>
+          <Card></Card>
+          <ImageCard></ImageCard>
+          <Card></Card>
+          <ImageCard></ImageCard>
+          <Card></Card>
+      </div>
     </div>
   );
 }
